4acee65bc6d2d496b6a3e1dd4083e1a598434a2d
Emre Tekgül <Emre.Tekguel@Student.HTW-Berlin.de>
2024-05-12 16:18:02 +0200

    Unterkomponente

---
+++ webtech-frontend/src/views/Spiele.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'Spiele '
+}
+</script>
+
+<template>
+  <hq>Welcome to Spiele</hq>
+
+</template>
+
+<style scoped>
+hq {
+  text-align: center;
+  position: relative;
+  top: -300px;
+  transform: translateY(-75%);
+  left: 50px;
+  right: 0;
+  margin: auto;
+  color: #69c86a; /* Ändern Sie diesen Wert, um die Farbe der Schrift zu ändern */
+  font-weight: bold; /* Ändern Sie diesen Wert, um die Dicke der Schrift zu ändern */
+}
+</style>
